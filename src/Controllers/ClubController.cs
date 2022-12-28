12826ec449b4ed2b4cdce27d92d0a4617125452f
levivanbulyk <levivanbulyk@gmail.com>
2022-12-28 15:10:04 +0100

    Added html and data from DB to the Race page

--- src/Controllers/ClubController.cs
+++ src/Controllers/ClubController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RunGroupWebApp.Data;
-using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Controllers
 {
--- src/Controllers/RaceController.cs
+++ src/Controllers/RaceController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
 
 namespace RunGroupWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly AppDBContext _context;
+        public RaceController(AppDBContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
