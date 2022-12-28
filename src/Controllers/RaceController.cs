6225d7c5dec1ce6de9ee52457c39d32de24a7a67
levivanbulyk <levivanbulyk@gmail.com>
2022-12-28 20:48:28 +0100

    New View added and RaceController modified for Race creation

--- src/Controllers/RaceController.cs
+++ src/Controllers/RaceController.cs
@@ -25,5 +25,21 @@ namespace RunGroupWebApp.Controllers
             Race race = await _raceRepository.GetByIdAsync(id);
             return View(race);
         }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Race race)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(race);
+            }
+            _raceRepository.Add(race);
+            return RedirectToAction("Index");
+        }
     }
 }
