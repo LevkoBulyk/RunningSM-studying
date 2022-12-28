4a7cce7da8d3be4548bf80b760b5ca10e0eedbac
levivanbulyk <levivanbulyk@gmail.com>
2022-12-28 13:57:39 +0100

    In ClubController data was extracted from the DB

--- src/Controllers/ClubController.cs
+++ src/Controllers/ClubController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+using RunGroupWebApp.Models;
 
 namespace RunGroupWebApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly AppDBContext _context;
+        public ClubController(AppDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
